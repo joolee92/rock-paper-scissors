d1d1891cd1a022a484339b8112147fce8b500fc1
Joo Lee <joo.lee08@qmail.cuny.edu>
2025-06-05 19:20:09 -0400

    Add getComputerChoice() function to randomize between rock, paper, or scissors

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Hello World");
+
+function getComputerChoice() {
+    const randomInt = Math.random();
+    if (randomInt < 0.33) return "Rock";
+    else if (randomInt < 0.66) return "Paper";
+    else return "Scissors";
+}
+
+console.log(getComputerChoice());
